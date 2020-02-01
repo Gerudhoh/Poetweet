24599b666ea5ee7b2e261a6c3ed7c0c96365d283
Julia Hohenadel <juliahoh@live.com>
2020-02-01 10:16:35 -0500

    Prints the welcome message! Time to start a user story :)

--- src/main/java/poetweet/Menu.java
+++ src/main/java/poetweet/Menu.java
@@ -1,6 +1,26 @@
 package poetweet;
 
 public class Menu {
+
+    /**
+     * Prints the welcome message
+     */
+    public void printWelcome(){
+        System.out.println("Welcome to");
+        System.out.println("   .-.                                                 ");
+        System.out.println("  (_) )-.                /                         /   ");
+        System.out.println("     /   \\  .-._. .-.---/---`)    (   .-.   .-.---/--- ");
+        System.out.println("    /     )(   )./.-'_ /    /  .   )./.-'_./.-'_ /     ");
+        System.out.println(" .-/  `--'  `-' (__.' /    (_.' `-' (__.' (__.' /      ");
+        System.out.println("(_/                                                    ");
+    }
+
+    /**
+     * Tell the user all it can do with this beautiful system
+     *
+     * @return true If the user quits the program, else returns false.
+     */
+
     public boolean runProgram(){
         System.out.println("Welcome to Poetweet!");
         return true;
--- src/main/java/poetweet/Poetweet.java
+++ src/main/java/poetweet/Poetweet.java
@@ -3,11 +3,11 @@ package poetweet;
 public class Poetweet {
     /**
      * This is the main.
-     *
      */
     public static void main (String args[]) {
         Menu mainMenu = new Menu();
 
+        mainMenu.printWelcome();
         mainMenu.runProgram();
 
         /*while(mainMenu.getRunAgain() == true){
